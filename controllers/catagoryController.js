acfac62863f5c0f93615e7a5336da2f3b8a58c42
akberofh <sd85bejal@code.edu.az>
2025-02-21 22:32:34 +0400

    sfvsvdwwdff

--- controllers/catagoryController.js
+++ controllers/catagoryController.js
@@ -28,7 +28,7 @@ const catagoryAd = async (req, res) => {
 
 const catagoryUpdate = async (req, res) => {
     const { id } = req.params;
-   
+
     try {
 
 
@@ -61,30 +61,27 @@ const catagoryUpdate = async (req, res) => {
             _id: updatecatagory._id,
             title: updatecatagory.title,
             photo: updatecatagory.photo,
-          });
+        });
     } catch (error) {
         console.error(error);
         res.status(500).json({ message: error.message });
     }
 };
 
-
-
-
 const getCatagory = async (req, res) => {
     try {
-      const { page = 1, limit = 5 } = req.query; // Sayfa numarası ve limit parametrelerini alıyoruz, varsayılan olarak 1 ve 5
-      const skip = (page - 1) * limit; // Skip değeri, hangi kategorilerden başlayacağımızı hesaplıyor
+      const { page = 1, limit = 3 } = req.query; // Sayfa numarası ve limit parametrelerini alıyoruz, varsayılan olarak 1 ve 5
+      const skip = (page - 1) * limit; // Skip değeri, hangi ürünlerden başlayacağımızı hesaplıyor
   
-      const allCatagory = await CatagoryModel.find()
+      const allQolbaq = await CatagoryModel.find()
         .skip(skip) // Skip kullanarak başlamak istediğimiz veriyi belirliyoruz
-        .limit(limit); // Belirtilen limit kadar kategori getiriyoruz
+        .limit(limit); // Belirtilen limit kadar ürün getiriyoruz
   
-      const totalCatagoryCount = await CatagoryModel.countDocuments(); // Toplam kategori sayısını alıyoruz
-      const totalPages = Math.ceil(totalCatagoryCount / limit); // Toplam sayfa sayısını hesaplıyoruz
+      const totalQolbaqCount = await CatagoryModel.countDocuments(); // Toplam kayıt sayısını alıyoruz
+      const totalPages = Math.ceil(totalQolbaqCount / limit); // Toplam sayfa sayısını hesaplıyoruz
   
       res.json({
-        allCatagory,
+        allQolbaq,
         totalPages,
         currentPage: page,
       });
@@ -93,7 +90,7 @@ const getCatagory = async (req, res) => {
       res.status(500).json({ error: "Internal Server Error" });
     }
   };
-  
+
 
 const getByIdCatagory = async (req, res) => {
     const { id } = req.params;
